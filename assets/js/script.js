3b4a688148d51842f7d7643697995d39d98251ec
Karina June <116246298+KarinaJune@users.noreply.github.com>
2023-02-01 19:11:55 -0600

    updated apikey and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
-var apiKey = 'f93d3b10d3389db94f41066a5754a9f3';
+var apiKey = "f93d3b10d3389db94f41066a5754a9f3";
+
+var weatherUpdateUrl = `https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid=${apiKey}`;
+
+var btn = document.querySelector("#searchCity");
+
+
+function runApi () {
+    fetch(weatherUpdateUrl, {
+        mode: "cors", 
+        headers: {
+            "Access-Control-Allow-Origin": "*"
+        }
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        weatherCompare (data)
+      });
+
+    };
+
+
+
+
+
+btn.addEventListener("click", function() {
+    runApi ()
+});
+      
