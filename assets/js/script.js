7e724ad2d3db12e8c0315cf517dd5f0aad642246
Karina June <116246298+KarinaJune@users.noreply.github.com>
2023-02-03 17:20:55 -0600

    update the js code for api code. also renamed id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,46 @@
 var apiKey = "f93d3b10d3389db94f41066a5754a9f3";
-
-var weatherUpdateUrl = `https://api.openweathermap.org/data/2.5/forecast?q={cityName}&appid=${apiKey}`;
-
 var btn = document.querySelector("#searchCity");
+var enteredCity = document.querySelector(".display")
 
 
 function runApi () {
-    fetch(weatherUpdateUrl, {
-        mode: "cors", 
-        headers: {
-            "Access-Control-Allow-Origin": "*"
-        }
-    })
+  var weatherUpdateUrl = `https://api.openweathermap.org/data/2.5/forecast?q=phoenix&appid=${apiKey}`;
+
+    fetch(weatherUpdateUrl)
       .then(function (response) {
         return response.json();
       })
       .then(function (data) {
         console.log(data);
-        weatherCompare (data)
+        info(data)
       });
+};
 
+
+function info(data) {
+  var cityInput = document.getElementById("cityInput").value;
+
+  for (var i = 0; i < data.length; i++) {
+    var theCity = data[i].city.name 
+    var weatherData = {
+    temp: data[i].main.temp,
+    realFeel: data[i].main.feels_like,
+    min: data[i].main.temp_min,
+    max: data[i].main.temp_max
     };
 
 
+    if (theCity === cityInput) {
+        addText (weatherData);
+        return
+    }
+    else{
+        enteredCity.innerHTML = "No Results Found"
+    }
+
+
+  }
+};
 
 
 
@@ -30,3 +48,8 @@ btn.addEventListener("click", function() {
     runApi ()
 });
       
+function addText (weatherData) {
+  enteredCity.innerHTML = weatherData.temp + " " + weatherData.realFeel + " " + weatherData.min + " " + weatherData.max + " "
+};
+
+
